895c0fd48cbed5f9b1c57d25a3304cef737db8ea
AndreDilay <51123739+AndreDilay@users.noreply.github.com>
2019-05-30 21:34:18 -0300

    Camera agora segue o player
    podendo também seguir outras coisa
    
    baseado no vídeo : https://www.youtube.com/watch?v=U66VYM-ShVg

---
+++ Projeto TMD/Assets/Scripts/CameraSeguir.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguir : MonoBehaviour
+{
+
+    private Func<Vector3> GetCameraSeguirPosicaoFunc;
+
+
+    //Func para camera (não sei como funciona Func)
+    public void Config(Func<Vector3> GetCameraSeguirPosicaoFunc)
+    {
+        this.GetCameraSeguirPosicaoFunc = GetCameraSeguirPosicaoFunc;
+    }
+
+
+    //Metodo para mudar oq a camera segue
+    public void SetGetCameraSeguirPosicaoFunc(Func<Vector3> GetCameraSeguirPosicaoFunc)
+    {
+        this.GetCameraSeguirPosicaoFunc = GetCameraSeguirPosicaoFunc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Achar a posicao da camera
+        Vector3 cameraSeguirPosicao = GetCameraSeguirPosicaoFunc();
+        cameraSeguirPosicao.z = transform.position.z;
+
+        //Para Suavizar o movimento da camera
+        Vector3 cameraMovDir = (cameraSeguirPosicao - transform.position).normalized;
+        float distancia = Vector3.Distance(cameraSeguirPosicao, transform.position);
+        float cameraVeloMov = 3f;
+
+
+        //Para não bugar com baixo Framerate
+        if (distancia > 0)
+        {
+            Vector3 novaPosicaoCamera = transform.position = transform.position + cameraMovDir * distancia * cameraVeloMov * Time.deltaTime;
+
+            float distaciaDepoisDeMover = Vector3.Distance(novaPosicaoCamera, cameraSeguirPosicao);
+
+            if (distaciaDepoisDeMover > distancia)
+            {
+                //Ultrapassou o alvo
+                novaPosicaoCamera = cameraSeguirPosicao;
+            }
+
+            transform.position = novaPosicaoCamera;
+        }    
+        
+
+    }
+}
---
+++ Projeto TMD/Assets/Scripts/ControladorDeJogo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeJogo : MonoBehaviour
+{
+
+    [SerializeField] CameraSeguir cameraSeguir;
+    [SerializeField] Transform posicaoPlayer;
+
+    //Para Fazer a camera seguir outra coisa basta add uma referencia
+    // Transform nomeDaVariavel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraSeguir.Config(() => posicaoPlayer.position);
+
+        //Modificar esta linha para seguir a referencia
+        //cameraSeguir.SetGetCameraSeguirPosicaoFunc(() => nomeDaVariavel.position);
+        //Pode também criar botões para seguir diferentes referencias
+    }
+
+}
